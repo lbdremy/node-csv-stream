3739858243c61cd66a9874e1e4fad478c896297f
Remy Loubradou <remyloubradou@gmail.com>
2012-08-30 12:54:33 +0100

    FIX: Use tester instead of fs to pipe data into my stream.

--- test/stream-spec.js
+++ test/stream-spec.js
@@ -16,7 +16,7 @@ spec(stream)
 	.validateOnExit();
 
 var first = false;
-/*
+
 tester.createRandomStream(function(){
 	if(first){
 		first = false;
@@ -25,7 +25,10 @@ tester.createRandomStream(function(){
 	var id = Math.random();
 	return new Buffer(  id + ',title' + id + ',description' + id + '\r\n' );
 },1000)
-*/
+	.pipe(stream)
+	.pipe(tester.createPauseStream());
+/*
 require('fs').createReadStream('/home/lbdremy/Downloads/productBrands.csv')
 	.pipe(stream)
 	.pipe(tester.createPauseStream());
+*/
